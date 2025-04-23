6b32242a7ff644c0aec2060bc968ed65114247c0
Catalina <catalina.correa346@pascualbravo.edu.co>
2025-04-23 17:24:13 -0500

    feat: agregar clases de entrega: Dron, Moto y Camión

---
+++ PedidosApp/EntregarCamion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public class EntregarCamion : IMetodoEntrega
+    {
+        public double CalcularCosto(int km) => 5 * km;
+        public string TipoEntrega() => "Camión";
+    }
+}
