76405d01d127212f6ae5291b8c660829823aa763
Catalina <catalina.correa346@pascualbravo.edu.co>
2025-04-23 17:16:09 -0500

    Creación del diseño del form

--- PedidosApp/Form1.Designer.cs
+++ PedidosApp/Form1.Designer.cs
@@ -33,80 +33,135 @@
             this.chkUrgente = new System.Windows.Forms.CheckBox();
             this.nudPeso = new System.Windows.Forms.NumericUpDown();
             this.nudDistancia = new System.Windows.Forms.NumericUpDown();
+            this.lblResultado = new System.Windows.Forms.Label();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.nudPeso)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudDistancia)).BeginInit();
             this.SuspendLayout();
             // 
             // txtCliente
             // 
-            this.txtCliente.Location = new System.Drawing.Point(202, 68);
+            this.txtCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCliente.Location = new System.Drawing.Point(207, 61);
             this.txtCliente.Name = "txtCliente";
-            this.txtCliente.Size = new System.Drawing.Size(100, 22);
+            this.txtCliente.Size = new System.Drawing.Size(141, 27);
             this.txtCliente.TabIndex = 0;
             // 
             // cmbProducto
             // 
+            this.cmbProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbProducto.FormattingEnabled = true;
             this.cmbProducto.Items.AddRange(new object[] {
             "Tecnología",
             "Accesorio",
             "Componente"});
-            this.cmbProducto.Location = new System.Drawing.Point(202, 126);
+            this.cmbProducto.Location = new System.Drawing.Point(207, 108);
             this.cmbProducto.Name = "cmbProducto";
-            this.cmbProducto.Size = new System.Drawing.Size(121, 24);
+            this.cmbProducto.Size = new System.Drawing.Size(141, 28);
             this.cmbProducto.TabIndex = 1;
             // 
             // chkUrgente
             // 
             this.chkUrgente.AutoSize = true;
-            this.chkUrgente.Location = new System.Drawing.Point(207, 191);
+            this.chkUrgente.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkUrgente.Location = new System.Drawing.Point(153, 165);
             this.chkUrgente.Name = "chkUrgente";
-            this.chkUrgente.Size = new System.Drawing.Size(91, 20);
+            this.chkUrgente.Size = new System.Drawing.Size(118, 24);
             this.chkUrgente.TabIndex = 2;
             this.chkUrgente.Text = "¿Urgente?";
             this.chkUrgente.UseVisualStyleBackColor = true;
             // 
             // nudPeso
             // 
-            this.nudPeso.Location = new System.Drawing.Point(207, 239);
+            this.nudPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudPeso.Location = new System.Drawing.Point(578, 59);
             this.nudPeso.Name = "nudPeso";
-            this.nudPeso.Size = new System.Drawing.Size(120, 22);
+            this.nudPeso.Size = new System.Drawing.Size(120, 27);
             this.nudPeso.TabIndex = 3;
             // 
             // nudDistancia
             // 
-            this.nudDistancia.Location = new System.Drawing.Point(207, 282);
+            this.nudDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudDistancia.Location = new System.Drawing.Point(578, 114);
             this.nudDistancia.Name = "nudDistancia";
-            this.nudDistancia.Size = new System.Drawing.Size(120, 22);
+            this.nudDistancia.Size = new System.Drawing.Size(120, 27);
             this.nudDistancia.TabIndex = 4;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(448, 211);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 20);
+            this.lblResultado.TabIndex = 6;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(60, 329);
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(198, 202);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
-            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Size = new System.Drawing.Size(160, 45);
+            this.btnCalcular.TabIndex = 7;
             this.btnCalcular.Text = "CALCULAR";
             this.btnCalcular.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(415, 336);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nombre del cliente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Producto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(404, 61);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(167, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Peso del producto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(404, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Distancia en km:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 289);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.nudDistancia);
             this.Controls.Add(this.nudPeso);
             this.Controls.Add(this.chkUrgente);
@@ -114,6 +169,7 @@
             this.Controls.Add(this.txtCliente);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.nudPeso)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudDistancia)).EndInit();
             this.ResumeLayout(false);
@@ -128,8 +184,12 @@
         private System.Windows.Forms.CheckBox chkUrgente;
         private System.Windows.Forms.NumericUpDown nudPeso;
         private System.Windows.Forms.NumericUpDown nudDistancia;
+        private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
